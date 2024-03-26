1431b7ec3d358014c6ca15e6cb8ea57f2ff54d34
Mahdi Behofatdeh - Work Station <mahdi.behoftadeh.1381@gmail.com>
2024-03-26 17:17:40 +0330

    [ add ] Add router to index file

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const Database = require('./Configs/Database')
+const fs = require('fs');
+const path = require('path');
+const BodyParser = require('body-parser');
 
-const router = require('./routes/api')
-app.use('/api/v1', router)
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: false }));
+
+const database = Database.connect()
+// Read directory
+const dirPath = path.join(__dirname, 'Routes');
+fs.readdirSync(dirPath).forEach(file => {
+    const filePath = "./Routes/" + file.replace('.js', '')
+    const route = require(filePath)
+    app.use(process.env.API_ROUTE_PREFIX + '/' + route.ROUTE_NAME, route.router)
+});
+
 app.listen(3000, () => console.log('Server started and listening on port 3000'))
