beb1cb4b8ca5645194c3c60fbac9fcd358d17d42
Mahdi Behofatdeh - Work Station <mahdi.behoftadeh.1381@gmail.com>
2024-03-26 17:23:02 +0330

    [ add ] Add CRUD functions to note controller

--- Controllers/NotesController.js
+++ Controllers/NotesController.js
@@ -1,49 +1,133 @@
-const index = (request, response, next) => {
+const Database = require('../Configs/Database')
+const Note = require('../Models/Note')
+
+const index = async (request, response, next) => {
     try{
-        response.status(200).json({ message: "ok" })
-    }catch (err){
-        response.status(500).json({ message: "nok" })
+        const notes = await Note.find()
+        return response.status(200).json({
+            status: 200,
+            message: "Operation successful.",
+            data: notes
+        })
+    }catch (error){
+        return response.status(500).json({
+            status: 500,
+            message: error.message,
+            data: null
+        })
     }
 }
 
-const show = (request, response, next) => {
+const show = async (request, response, next) => {
     try{
-        response.status(200).json({
-            param: request.params,
-            message: "ok" })
-    }catch (err){
-
+        const noteExists = await Note.exists({_id: request.params.id});
+        if (!noteExists){
+            return response.status(404).json({
+                status: 404,
+                message: "Note not found.",
+                data: null
+            })
+        }
+        const note = await Note.findById(request.params.id)
+        if (!note){
+            return response.status(404).json({
+                status: 404,
+                message: "Note not found.",
+                data: null
+            })
+        }
+        return response.status(200).json({
+            status: 200,
+            message: "Operation successful.",
+            data: note
+        })
+    }catch (error){
+        return response.status(500).json({
+            status: 500,
+            message: error.message,
+            data: null
+        })
     }
 }
 
-const store = (request, response, next) => {
+
+const store = async (request, response, next) => {
     try{
-
-    }catch (err){
-
+        const note = new Note({
+            title: request.body.title,
+            text: request.body.text,
+        })
+        const savedNote = await note.save()
+        return response.status(201).json({
+            status: 201,
+            message: "Note created successfully.",
+            data: savedNote
+        })
+    }catch (error){
+        return response.status(400).json({
+            status: 400,
+            message: error.message,
+            data: null
+        })
     }
 }
 
-const update = (request, response, next) => {
+const update = async (request, response, next) => {
     try{
-
-    }catch (err){
-
+        const noteExists = await Note.exists({_id: request.params.id});
+        if (!noteExists){
+            return response.status(404).json({
+                status: 404,
+                message: "Note not found.",
+                data: null
+            })
+        }
+        await Note.findOneAndUpdate(
+            {
+                _id: request.params.id
+            },
+            {
+                title: request.body.title,
+                text: request.body.text
+            })
+        const updatedNote = await Note.findById(request.params.id)
+        return response.status(200).json({
+            status: 200,
+            message: "Note updated successfully.",
+            data: updatedNote
+        })
+    }catch (error){
+        return response.status(404).json({
+            status: 404,
+            message: "Note not found. " + error.message,
+            data: null
+        })
     }
 }
 
-const destroy = (request, response, next) => {
+const destroy = async (request, response, next) => {
     try{
-
-    }catch (err){
-
+        const noteExists = await Note.exists({_id: request.params.id});
+        if (!noteExists){
+            return response.status(404).json({
+                status: 404,
+                message: "Note not found.",
+                data: null
+            })
+        }
+        const deletedNote = await Note.findByIdAndDelete(request.params.id)
+        return response.status(200).json({
+            status: 200,
+            message: "Note deleted successfully.",
+            data: deletedNote
+        })
+    }catch (error){
+        return response.status(404).json({
+            status: 404,
+            message: "Note not found. " + error.message,
+            data: null
+        })
     }
 }
-
-// module.exports = index
-// module.exports = show
-// module.exports = store
-// module.exports = update
-// module.exports = destroy
 
 module.exports = {index, show, store, update, destroy}
